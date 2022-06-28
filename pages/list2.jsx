eb4d9fe87cf5da9b875ee2a884762b5bdf059c46
higorcos <higorcosta2001@outlook.com>
2022-06-28 11:21:41 -0300

    init

---
+++ pages/list2.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+export default function MyApp() {
+  return(
+    <>
+    <Head>
+            <title>Notícias 02</title>             
+            <meta property="fb:app_id" content="00"/>
+            <meta property="og:title" content='Noticias/02'/>
+            <meta property="og:description" content="Fique por dentro das informações"/>
+            <meta property="og:type" content="article" />
+            <meta property="og:url" content="https://www.raposa.ma.gov.br"/>
+            <meta property="og:site_name" content="raposa.ma.gov.br"/>
+            <meta property="og:image" content='https://api-s3-tss.s3.amazonaws.com/27-6-2022%205:28:19Captura%20de%20tela%20de%202022-06-27%2002-27-10.png'/>
+    </Head>
+        <h1>oooooo</h1>
+    </>
+  )
+}
